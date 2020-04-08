a7ac11edf678814b95f29068afdf35ceda5d0805
Patryk <58995560+PatrykJurczyk@users.noreply.github.com>
2020-04-08 17:02:16 +0200

    Delete 1.Zadanie_pierwsze.py

--- 1.Zadanie_pierwsze.py
+++
@@ -1,6 +0,0 @@
-# Zad. 1 
-# Napisz skrypt, który pobiera od użytkownika zdanie i liczy w nim spacje.
-# Wynik wyświetla na ekranie (użyj instrukcji input)
-
-a = input("Podaj zdanie z pieciu slow: \n")
-print(a.count(" "))
