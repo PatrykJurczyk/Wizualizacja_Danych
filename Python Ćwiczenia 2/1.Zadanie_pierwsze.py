596d229399ef7434dac18337e8abfd5917eda7a0
Patryk <58995560+PatrykJurczyk@users.noreply.github.com>
2020-04-08 17:03:16 +0200

    Add files via upload

---
+++ Python Ćwiczenia 2/1.Zadanie_pierwsze.py
@@ -0,0 +1,6 @@
+# Zad. 1 
+# Napisz skrypt, który pobiera od użytkownika zdanie i liczy w nim spacje.
+# Wynik wyświetla na ekranie (użyj instrukcji input)
+
+a = input("Podaj zdanie z pieciu slow: \n")
+print(a.count(" "))
---
+++ Python Ćwiczenia 2/7.Zadanie_siodme.py
@@ -0,0 +1,9 @@
+# Zad. 7 
+# Napisz pętle, która pobiera liczby od
+# użytkownika i wyświetla ich kwadraty na ekranie.
+from math import * 
+
+
+for x in input("Podaj liczbe: "):
+    z = pow(int(x), 2)
+    print(z)
