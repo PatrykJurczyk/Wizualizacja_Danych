86e042889aecd7b812c4adab82fb9d91ba42cf62
Patryk <58995560+PatrykJurczyk@users.noreply.github.com>
2020-04-08 16:59:21 +0200

    Delete 7.Zadanie_siodme.py

--- 7.Zadanie_siodme.py
+++
@@ -1,9 +0,0 @@
-# Zad. 7 
-# Napisz pętle, która pobiera liczby od
-# użytkownika i wyświetla ich kwadraty na ekranie.
-from math import * 
-
-
-for x in input("Podaj liczbe: "):
-    z = pow(int(x), 2)
-    print(z)
