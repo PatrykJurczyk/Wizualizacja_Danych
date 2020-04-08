dabee8f56223f19f36c2dbe3c911812602b8af25
Patryk <58995560+PatrykJurczyk@users.noreply.github.com>
2020-04-08 17:00:52 +0200

    Delete 6.Zadanie_szoste.py

--- 6.Zadanie_szoste.py
+++
@@ -1,9 +0,0 @@
-# Zad. 6 
-# Napisz pętlę, 
-# która wyświetla liczby podzielne przez 5 
-
-#Zapytać sie czy da sie napsać ze 5 liczb a 
-# nie ograniczac ich do danej liczby tak jak w cpp for(int i=0; i<5; i++).
-
-for x in range(0,24,5):
-    print(str(x) + " ")
